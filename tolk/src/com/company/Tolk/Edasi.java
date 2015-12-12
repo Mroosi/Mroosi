cb6a24d4234a7cff32394ca6b1de0a8998c27abe
Mroosi <madisroosi@hotmail.com>
2015-12-12 20:52:44 +0200

    Palju asju juurde tehtud

--- tolk/src/com/company/Tolk/Edasi.java
+++ tolk/src/com/company/Tolk/Edasi.java
@@ -4,6 +4,14 @@ import java.util.HashMap;
 
 
 public class Edasi {
+    int vot;
+    String siss;
+    public Edasi(int myVot, String mySiss) {
+        vot = myVot;
+        siss = mySiss;
+    }
+
+
 
     public String[] tahestik = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
             "O", "P", "R", "S", "Z", "T", "U", "V", "W", "X", "Y", "Q", " ", "1", "2", "3", "4", "5", "6",
@@ -12,8 +20,10 @@ public class Edasi {
     public String ed() {
         String vastus = "";
         System.out.println(vastus);
-        int v = 1;                        /*Siia peaks tulema võti kasutaja sisendist*/
-        String tekst = "abc";          /*Siia peaks tulema tekst kasutaja sisendist*/
+        int v = vot;
+        System.out.println(siss);
+        System.out.println(vot);
+        String tekst = siss;
         int pikkus = tekst.length();
         HashMap<String, String> t2hestik = new HashMap<String, String>();
         for (int i = 0; i <tahestik.length; i++) {
@@ -22,6 +32,15 @@ public class Edasi {
                 t=t-tahestik.length;
             }
             t2hestik.put(tahestik[i],tahestik[t]);
+            for (int indeks = 0; indeks < pikkus; indeks++) {
+                char m = tekst.charAt(indeks);
+                String kontroll = Character.toString(m);
+                String su = kontroll.toUpperCase();
+                if (!t2hestik.containsKey(su)){
+                    t2hestik.put(su,su);
+                }
+
+            }
         }
         for (int i = 0; i < pikkus; i++) {
             char m = tekst.charAt(i);
--- tolk/src/com/company/Tolk/Main.java
+++ tolk/src/com/company/Tolk/Main.java
@@ -6,9 +6,14 @@ import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class Main extends Application {
-
+    String salvestis = "";
     @Override
     public void start(Stage primaryStage) throws Exception {
         VBox vbox = new VBox();
@@ -24,54 +29,83 @@ public class Main extends Application {
         Label votmeLabel = new Label("Sisesta krypteerimise voti numbrites");
         PasswordField votmeSisend = new PasswordField();
         ToggleGroup grupp = new ToggleGroup();
-        ToggleButton krypteeri = new ToggleButton("Krypteeri    ");
-        ToggleButton dekrypteeri = new ToggleButton("Dekrypteeri");
-        ToggleButton info = new ToggleButton("   Info   ");
-        ToggleButton exit = new ToggleButton("Exit");
+        ToggleButton krypteeri = new ToggleButton("     Krypteeri     ");
+        ToggleButton dekrypteeri = new ToggleButton("   Dekrypteeri   ");
+        ToggleButton save = new ToggleButton("        Save         ");
+        ToggleButton info = new ToggleButton("         Info          ");
+        ToggleButton exit = new ToggleButton("         Exit          ");
+        ToggleButton puhasta = new ToggleButton(" Puhasta valjad ");
         krypteeri.setToggleGroup(grupp);
         dekrypteeri.setToggleGroup(grupp);
         exit.setToggleGroup(grupp);
+        puhasta.setToggleGroup(grupp);
         info.setToggleGroup(grupp);
+        save.setToggleGroup(grupp);
         Label teade = new Label();
-        /*TextArea*/
 
-        vbox.getChildren().addAll(votmeLabel, votmeSisend, sisendTekst, kasutajaSisend, valjundTekst, valjund, krypteeri, dekrypteeri, info,  exit,teade);
+        vbox.getChildren().addAll(votmeLabel, votmeSisend, sisendTekst, kasutajaSisend, valjundTekst, valjund, krypteeri, dekrypteeri, save, puhasta,  info, exit, teade);
 
         krypteeri.setOnAction(event -> {
             int kryptVoti = 0;
-
-            String sisse = kasutajaSisend.getText();  /*Tahaks siit saada need väärtused ja kasutada neid Edasi klassis*/
+            String sisse = kasutajaSisend.getText();
             String voti = votmeSisend.getText();
             try {
                 kryptVoti = Integer.parseInt(voti);
             } catch (Exception e) {
                 teade.setText("Sisesta krypteerimise voti numbrites");
             }
-
-            Edasi k = new Edasi();
+            int vot = kryptVoti;
+            String siss = sisse;
+            System.out.println(vot);
+            System.out.println(siss);
+            Edasi k = new Edasi(vot, siss);
             k.ed();
             String vastus = k.ed();
             valjund.setText(vastus);
-
-
+            salvestis = vastus;
         });
         dekrypteeri.setOnAction(event -> {
             int kryptVoti = 0;
-
-            String sisse = kasutajaSisend.getText();  /*Tahaks siit saada need väärtused ja kasutada neid Tagasi klassis*/
+            String sisse = kasutajaSisend.getText();
             String voti = votmeSisend.getText();
             try {
                 kryptVoti = Integer.parseInt(voti);
             } catch (Exception e) {
                 teade.setText("Sisesta krypteerimise voti numbrites");
             }
-
-            Tagasi t = new Tagasi();
+            int vot = kryptVoti;
+            String siss = sisse;
+            System.out.println(vot);
+            System.out.println(siss);
+            Tagasi t = new Tagasi(vot, siss);
             t.tag();
             String vastuTagasi = t.tag();
             valjund.setText(vastuTagasi);
+        });
+        save.setOnAction(event -> {
 
+            try
+            {
+                File file = new File("SKM");
+                FileWriter fw = new FileWriter( file.getAbsoluteFile( ) );
+                BufferedWriter bw = new BufferedWriter( fw );
+                bw.write(salvestis);
+                bw.close( );
+                teade.setText("Salvestatud: " + (file.getAbsolutePath()));
+            }
+            catch( IOException e )
+            {
+                System.out.println("Error: " + e);
+                e.printStackTrace( );
+                teade.setText("Errrrrrrr");
+            }
 
+        });
+        puhasta.setOnAction(event -> {
+            teade.setText("");
+            valjund.setText("");
+            votmeSisend.setText("");
+            kasutajaSisend.setText("");
         });
         info.setOnAction(event -> {
             teade.setText("Siia tuleb info programmi kohta ja kasutusjuhend");
@@ -79,6 +113,7 @@ public class Main extends Application {
         exit.setOnAction(event -> {
             System.exit(0);
         });
+
     }
 
 
